2813eb883146ff49fd604f608b2a4c29cd6fd505
RVASHISHT <rohan.vashisht@snowflake.com>
2026-07-14 07:27:09 +0000

    updating to account for new macro approach

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -1,10 +1,9 @@
 {% macro generate_schema_name(custom_schema_name, node) -%}
   {%- set dev_suffix = var('dev_suffix', '') -%}
+  {%- set schema = custom_schema_name | trim if custom_schema_name else target.schema -%}
   {%- if dev_suffix != '' -%}
-    {{ target.schema }}_{{ dev_suffix }}
-  {%- elif custom_schema_name is none -%}
-    {{ target.schema }}
+    {{ dev_suffix }}_{{ schema }}
   {%- else -%}
-    {{ custom_schema_name | trim }}
+    {{ schema }}
   {%- endif -%}
 {%- endmacro %}
